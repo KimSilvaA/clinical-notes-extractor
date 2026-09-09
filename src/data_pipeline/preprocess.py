70b883b41108bc6a0877fe4533af7ada8c7c5601
aks <124469197+KimSilvaA@users.noreply.github.com>
2026-09-09 09:38:13 -0400

    Made first version of pipeline

--- src/data_pipeline/preprocess.py
+++ src/data_pipeline/preprocess.py
@@ -35,17 +35,14 @@ def clean_data(data_list):
         "notes": data_list[1:]
 }
 
-# clean_data_list = clean_data(data_list)
-# data_json = json.dumps(clean_data_list)
+clean_data_list = clean_data(data_list)
+data_json = json.dumps(clean_data_list)
 
 fhir_files = ['Abe604_Frami345_b8dd1798-beef-094d-1be4-f90ee0e6b7d5.json',
               'Abraham100_Stracke611_990db43c-4604-a27c-4153-bfa9ef42a9e2.json',
               'Zachary28_Ankunding277_5442450f-35c1-85e9-3b93-e7047cd9813e.json',
               'Zack583_Runte676_2b073598-a7c1-9d50-eb8e-7ffc816d2b91.json',
               'Zane918_Cruickshank494_1b88b604-43a9-dfcb-d0e3-f4fe47eac581.json'
-
-
-
 
               ]
 def main():
